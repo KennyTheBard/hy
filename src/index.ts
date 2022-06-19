88f81571a1ca3b3a49ba5f9510a4b2da272a41f1
KennyTheBard <octav_teculescu@yahoo.com>
2022-06-19 12:18:17 +0300

    Fixed prompts

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,15 @@ import os from 'os';
 import * as fs from 'fs';
 import { runner, Logger } from 'hygen';
 import { execa } from 'execa';
+import enquirer from 'enquirer';
+import { Prompter } from 'hygen/dist/types';
 
 const CONFIG_DIR_PATH = path.join(os.homedir(), '.hy');
 
 (async () => {
    if (!fs.existsSync(CONFIG_DIR_PATH)) {
       fs.mkdirSync(CONFIG_DIR_PATH);
+      // await runner 
    }
    const defaultTemplates = path.join(CONFIG_DIR_PATH, '_templates')
    const { success } = await runner(process.argv.slice(2), {
@@ -20,8 +23,10 @@ const CONFIG_DIR_PATH = path.join(os.homedir(), '.hy');
          const opts = body && body.length > 0 ? { input: body } : {}
          return execa(action, { ...opts, shell: true })
       },
-      createPrompter: () => require('enquirer'),
+      createPrompter: <Q, T>() => ({
+         prompt: enquirer.prompt
+      } as unknown as Prompter<Q, T>),
    });
-   
+
    process.exit(success ? 0 : 1)
 })();
