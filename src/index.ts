c8eed1b487d7deb4b623a37aea82178ee3578698
KennyTheBard <octav_teculescu@yahoo.com>
2022-06-19 16:37:40 +0300

    Added home folder initialization step

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,16 @@ import { runner, Logger } from 'hygen';
 import { execa } from 'execa';
 import enquirer from 'enquirer';
 import { Prompter } from 'hygen/dist/types';
+import { chdir } from 'node:process';
 
 const CONFIG_DIR_PATH = path.join(os.homedir(), '.hy');
 
 (async () => {
-   if (!fs.existsSync(CONFIG_DIR_PATH)) {
-      fs.mkdirSync(CONFIG_DIR_PATH);
-      // await runner 
-   }
-   const defaultTemplates = path.join(CONFIG_DIR_PATH, '_templates')
-   const { success } = await runner(process.argv.slice(2), {
+   const defaultTemplates = path.join(CONFIG_DIR_PATH, '_templates');
+   const cwd = process.cwd();
+   const runnerConfig = {
       templates: defaultTemplates,
-      cwd: process.cwd(),
+      cwd: cwd,
       logger: new Logger(console.log.bind(console)),
       debug: !!process.env.DEBUG,
       exec: (action, body) => {
@@ -26,7 +24,19 @@ const CONFIG_DIR_PATH = path.join(os.homedir(), '.hy');
       createPrompter: <Q, T>() => ({
          prompt: enquirer.prompt
       } as unknown as Prompter<Q, T>),
-   });
+   };
 
+   const missingHyDir = !fs.existsSync(CONFIG_DIR_PATH);
+   const missingTemplatesDir = !fs.existsSync(defaultTemplates);
+   if (missingHyDir || missingTemplatesDir) {
+      if (missingHyDir) {
+         fs.mkdirSync(CONFIG_DIR_PATH);
+      }
+      chdir(CONFIG_DIR_PATH);
+      await execa('hygen', ['init', 'self']);
+      chdir(cwd);
+   }
+
+   const { success } = await runner(process.argv.slice(2), runnerConfig);
    process.exit(success ? 0 : 1)
 })();
