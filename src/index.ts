98dc74ed9fb08f70fb0434b4ca7b9a36da1f9f91
KennyTheBard <octav_teculescu@yahoo.com>
2022-07-22 19:41:08 +0300

    Fixed it

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,32 @@
 import path from 'path';
 import os from 'os';
 import * as fs from 'fs';
-import { runner, Logger } from 'hygen';
 import { execa } from 'execa';
-import enquirer from 'enquirer';
-import { Prompter } from 'hygen/dist/types';
-import { chdir } from 'node:process';
 
 const CONFIG_DIR_PATH = path.join(os.homedir(), '.hy');
+const defaultTemplates = path.join(CONFIG_DIR_PATH, '_templates');
+const cwd = process.cwd();
 
 (async () => {
-   const defaultTemplates = path.join(CONFIG_DIR_PATH, '_templates');
-   const cwd = process.cwd();
-   const runnerConfig = {
-      templates: defaultTemplates,
-      cwd: cwd,
-      logger: new Logger(console.log.bind(console)),
-      debug: !!process.env.DEBUG,
-      exec: (action, body) => {
-         const opts = body && body.length > 0 ? { input: body } : {}
-         return execa(action, { ...opts, shell: true })
-      },
-      createPrompter: <Q, T>() => ({
-         prompt: enquirer.prompt
-      } as unknown as Prompter<Q, T>),
-   };
-
+   // check if the global templates directory has been initialized
    const missingHyDir = !fs.existsSync(CONFIG_DIR_PATH);
    const missingTemplatesDir = !fs.existsSync(defaultTemplates);
    if (missingHyDir || missingTemplatesDir) {
       if (missingHyDir) {
          fs.mkdirSync(CONFIG_DIR_PATH);
       }
-      chdir(CONFIG_DIR_PATH);
+      process.chdir(CONFIG_DIR_PATH);
       await execa('hygen', ['init', 'self']);
-      chdir(cwd);
+      process.chdir(cwd);
    }
 
-   const { success } = await runner(process.argv.slice(2), runnerConfig);
-   process.exit(success ? 0 : 1)
+   try {
+      await execa('hygen', ['init', 'self'], {
+         env: {
+            HYGEN_TMPLS: defaultTemplates
+         }
+      });
+   } catch (e) {
+      console.error(e);
+   }
 })();
